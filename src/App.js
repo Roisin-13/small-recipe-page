63805a645588d5ee8db48d82eec55c148625c5be
Roisin-13 <Roisin-13@users.noreply.github.com>
2024-10-22 18:40:31 +0100

    finished project all files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import BakeKit from './components/kit';
+import Method from './components/instructions';
+import Ingredients from './components/ingredients';
+import Footer from './components/footer';
+import insta from './components/images/instaGallery.png';
+import data from './components/recipe.json'
+import styled from 'styled-components';
+
+const Page = styled.div
+  ` background-color: #f7f2eb;
+  font-family: 'Ubuntu', sans-serif;`;
+
+const GridContainer = styled.div
+`  max-width: 1350px;
+  background: white;
+  display: grid;
+  padding: 30px;
+  grid-template-columns: repeat(9, 1fr);
+  grid-template-rows: repeat(4, 275px);
+  grid-gap: 30px;
+  margin: auto;`;
+
+const Header = styled.header
+`     background-color: #e15a97;
+      padding: 30px 0;
+      display: flex;
+      justify-content: center;`;
+
+const Title = styled.h1
+`   color: #ffffff;
+    font-family: "Alex Brush", cursive;
+    font-weight: 700;
+    font-size: 70px
+`;
+const InstaLink = styled.a
+` grid-area: 4/1/5/3;
+    padding: 20px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Page>        
+          <Header>
+          <Title>{data.shortbread.name}</Title>
+          </Header>
+        <GridContainer>
+            <BakeKit/>
+            <Method />
+            <Ingredients />
+          <InstaLink href="/">
+            <img src={insta}></img>
+        </InstaLink>
+        </GridContainer>
+      </Page>
+      <Footer />
+    </>
   );
 }
 
